5c44f62d7a93e03ddaec27df8230192b60bdc241
de4dot <de4dot@gmail.com>
2019-08-21 23:49:18 +0200

    Verify max op count value

--- Iced.UnitTests/Intel/EncoderTests/OpCodeInfoTestCasesReader.cs
+++ Iced.UnitTests/Intel/EncoderTests/OpCodeInfoTestCasesReader.cs
@@ -132,6 +132,8 @@ namespace Iced.UnitTests.Intel.EncoderTests {
 							tc.Op3Kind = ToOpCodeOperandKind(opParts[3]);
 						if (opParts.Length >= 5)
 							tc.Op4Kind = ToOpCodeOperandKind(opParts[4]);
+						if (Iced.Intel.DecoderConstants.MaxOpCount != 5)
+							throw new InvalidOperationException("Invalid MaxOpCount value");
 						if (opParts.Length >= 6)
 							throw new InvalidOperationException($"Invalid number of operands: '{value}'");
 						break;
