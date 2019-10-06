90b54fb3c53b8847f179463586b174db4865d346
0xd4d <de4dot@gmail.com>
2019-10-06 20:38:57 +0200

    Update build files

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
+param([switch]$NoMsbuild, [switch]$NoTest, [switch]$NoCoverage)
+
 $ErrorActionPreference = 'Stop'
 
 $configuration = 'Release'
@@ -8,20 +10,55 @@ $netcore_tfm = 'netcoreapp3.0'
 #
 
 $env:MoreDefineConstants = ''
+$env:NoTargetFrameworkNet35 = ''
 
-msbuild -v:m -restore -t:Build -p:Configuration=$configuration
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($IsWindows) {
+	$useMsbuild = $true
+}
+else {
+	$useMsbuild = $false
+}
+if ($NoMsbuild) {
+	$useMsbuild = $false
+}
 
-if (!(Test-Path Iced.UnitTests\bin\$configuration\$netcore_tfm)) { throw "Invalid tfm: $netcore_tfm" }
-dotnet test -c $configuration -f $netcore_tfm -p:Exclude='\"[Iced]Iced.Intel.InstructionMemorySizes,[Iced]Iced.Intel.EncoderInternal.OpCodeHandlers,[Iced]Iced.Intel.InstructionInfoInternal.InfoHandlers,[Iced]Iced.Intel.MnemonicUtils,[Iced]Iced.Intel.InstructionOpCounts\"' -p:ExcludeByFile="$PWD\Iced\**\*.g.cs" -p:ExcludeByAttribute='ObsoleteAttribute' -p:CollectCoverage=true -p:CoverletOutputFormat=lcov --no-build Iced.UnitTests\Iced.UnitTests.csproj -- RunConfiguration.NoAutoReporters=true RunConfiguration.TargetPlatform=X64
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if (!$useMsbuild) {
+	# There are currently no net35 reference assemblies on nuget
+	$env:NoTargetFrameworkNet35 = 'true'
+}
+
+if ($useMsbuild) {
+	msbuild -v:m -restore -t:Build -p:Configuration=$configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+else {
+	dotnet build -v:m -c $configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
+if (!$NoTest) {
+	if (!(Test-Path Iced.UnitTests/bin/$configuration/$netcore_tfm)) { throw "Invalid tfm: $netcore_tfm" }
+	$collectCoverage = if ($NoCoverage) { '' } else { 'true' }
+	dotnet test -c $configuration -f $netcore_tfm -p:Exclude='\"[Iced]Iced.Intel.InstructionMemorySizes,[Iced]Iced.Intel.EncoderInternal.OpCodeHandlers,[Iced]Iced.Intel.InstructionInfoInternal.InfoHandlers,[Iced]Iced.Intel.MnemonicUtils,[Iced]Iced.Intel.InstructionOpCounts\"' -p:ExcludeByFile="$PWD\Iced\**\*.g.cs" -p:ExcludeByAttribute='ObsoleteAttribute' -p:CollectCoverage=$collectCoverage -p:CoverletOutputFormat=lcov --no-build Iced.UnitTests/Iced.UnitTests.csproj -- RunConfiguration.NoAutoReporters=true RunConfiguration.TargetPlatform=X64
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
 
 # Don't include the IVT in the final binary
 $env:MoreDefineConstants = 'IcedNoIVT'
-msbuild -v:m -t:Clean -p:Configuration=$configuration
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-msbuild -v:m -restore -t:Build -p:Configuration=$configuration Iced\Iced.csproj
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
-msbuild -v:m -t:Pack -p:Configuration=$configuration Iced\Iced.csproj
-if ($LASTEXITCODE) { exit $LASTEXITCODE }
+if ($useMsbuild) {
+	msbuild -v:m -t:Clean -p:Configuration=$configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+	msbuild -v:m -restore -t:Build -p:Configuration=$configuration Iced/Iced.csproj
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+	msbuild -v:m -t:Pack -p:Configuration=$configuration Iced/Iced.csproj
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+else {
+	dotnet clean -v:m -c $configuration
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+	dotnet pack -v:m -c $configuration Iced/Iced.csproj
+	if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+
 $env:MoreDefineConstants = ''
+$env:NoTargetFrameworkNet35 = ''
