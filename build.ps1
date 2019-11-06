91b19ca973a0897839c372bd725ed1224a5a7d09
0xd4d <de4dot@gmail.com>
2019-11-06 02:49:44 +0100

    Move files again

--- build.ps1
+++ build.ps1
@@ -14,27 +14,27 @@ if ($null -eq $IsWindows) {
 	$IsWindows = $true
 }
 
-dotnet build -v:m -c $configuration src/Intel/csharp/Iced.sln
+dotnet build -v:m -c $configuration src/csharp/Iced.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 if (!$NoTest) {
 	if ($IsWindows) {
-		& $HOME/.nuget/packages/xunit.runner.console/$xunitVersion/tools/$xunitNetTfmVersion/xunit.console.exe src/Intel/csharp/Iced.UnitTests/bin/$configuration/$net_tfm/Iced.UnitTests.dll -noappdomain
+		& $HOME/.nuget/packages/xunit.runner.console/$xunitVersion/tools/$xunitNetTfmVersion/xunit.console.exe src/csharp/Intel/Iced.UnitTests/bin/$configuration/$net_tfm/Iced.UnitTests.dll -noappdomain
 		if ($LASTEXITCODE) { exit $LASTEXITCODE }
-		& $HOME/.nuget/packages/xunit.runner.console/$xunitVersion/tools/$xunitNetTfmVersion/xunit.console.x86.exe src/Intel/csharp/Iced.UnitTests/bin/$configuration/$net_tfm/Iced.UnitTests.dll -noappdomain
+		& $HOME/.nuget/packages/xunit.runner.console/$xunitVersion/tools/$xunitNetTfmVersion/xunit.console.x86.exe src/csharp/Intel/Iced.UnitTests/bin/$configuration/$net_tfm/Iced.UnitTests.dll -noappdomain
 		if ($LASTEXITCODE) { exit $LASTEXITCODE }
 	}
-	if (!(Test-Path src/Intel/csharp/Iced.UnitTests/bin/$configuration/$netcore_tfm)) { throw "Invalid tfm: $netcore_tfm" }
+	if (!(Test-Path src/csharp/Intel/Iced.UnitTests/bin/$configuration/$netcore_tfm)) { throw "Invalid tfm: $netcore_tfm" }
 	$collectCoverage = if ($NoCoverage) { '' } else { 'true' }
-	dotnet test -c $configuration -f $netcore_tfm -p:Exclude='\"[Iced]Iced.Intel.InstructionMemorySizes,[Iced]Iced.Intel.EncoderInternal.OpCodeHandlers,[Iced]Iced.Intel.InstructionInfoInternal.InfoHandlers,[Iced]Iced.Intel.MnemonicUtils,[Iced]Iced.Intel.InstructionOpCounts\"' -p:ExcludeByFile="$PWD\src\Intel\csharp\Iced\**\*.g.cs" -p:ExcludeByAttribute='ObsoleteAttribute' -p:CollectCoverage=$collectCoverage -p:CoverletOutputFormat=lcov --no-build src/Intel/csharp/Iced.UnitTests/Iced.UnitTests.csproj -- RunConfiguration.NoAutoReporters=true RunConfiguration.TargetPlatform=X64
+	dotnet test -c $configuration -f $netcore_tfm -p:Exclude='\"[Iced]Iced.Intel.InstructionMemorySizes,[Iced]Iced.Intel.EncoderInternal.OpCodeHandlers,[Iced]Iced.Intel.InstructionInfoInternal.InfoHandlers,[Iced]Iced.Intel.MnemonicUtils,[Iced]Iced.Intel.InstructionOpCounts\"' -p:ExcludeByFile="$PWD\src\Intel\csharp\Iced\**\*.g.cs" -p:ExcludeByAttribute='ObsoleteAttribute' -p:CollectCoverage=$collectCoverage -p:CoverletOutputFormat=lcov --no-build src/csharp/Intel/Iced.UnitTests/Iced.UnitTests.csproj -- RunConfiguration.NoAutoReporters=true RunConfiguration.TargetPlatform=X64
 	if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
 
 # Don't include the IVT in the final binary
 $env:MoreDefineConstants = 'IcedNoIVT'
-dotnet clean -v:m -c $configuration src/Intel/csharp/Iced.sln
+dotnet clean -v:m -c $configuration src/csharp/Iced.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
-dotnet pack -v:m -c $configuration src/Intel/csharp/Iced/Iced.csproj
+dotnet pack -v:m -c $configuration src/csharp/Intel/Iced/Iced.csproj
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 
 $env:MoreDefineConstants = ''
--- src/csharp/Intel/Iced.UnitTests/Intel/PathUtils.cs
+++ src/csharp/Intel/Iced.UnitTests/Intel/PathUtils.cs
@@ -28,7 +28,7 @@ using System.Reflection;
 namespace Iced.UnitTests.Intel {
 	static class PathUtils {
 		public static string GetTestTextFilename(string filename, params string[] directories) {
-			var baseDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "..", "..", "..", "..", "UnitTests");
+			var baseDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "..", "..", "..", "..", "..", "UnitTests", "Intel");
 			baseDir = Path.Combine(new string[] { baseDir }.Concat(directories).ToArray());
 			return Path.GetFullPath(Path.Combine(baseDir, filename));
 		}
