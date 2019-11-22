75f31db53e881808c822f5d7208ea583c08d7fa4
0xd4d <de4dot@gmail.com>
2019-11-22 22:35:04 +0100

    Update build.rs

--- src/rust/iced-x86/build.rs
+++ src/rust/iced-x86/build.rs
@@ -2,13 +2,14 @@ extern crate rustc_version;
 use rustc_version::{version, Version};
 
 fn main() {
-	if version().unwrap() >= Version::parse("1.26.0").unwrap() {
+	let version = version().unwrap();
+	if version >= Version::parse("1.26.0").unwrap() {
 		println!("cargo:rustc-cfg=has_fused_iterator");
 	}
-	if version().unwrap() >= Version::parse("1.27.0").unwrap() {
+	if version >= Version::parse("1.27.0").unwrap() {
 		println!("cargo:rustc-cfg=has_must_use");
 	}
-	if version().unwrap() < Version::parse("1.36.0").unwrap() {
+	if version < Version::parse("1.36.0").unwrap() {
 		println!("cargo:rustc-cfg=use_std_mem_uninitialized");
 	}
 }
