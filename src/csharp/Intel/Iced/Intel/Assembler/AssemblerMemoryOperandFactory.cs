87e367ad33a439770ad73972ba139ce3de413f20
Alexandre Mutel <alexandre_mutel@live.com>
2020-01-10 20:47:18 +0100

    Add support for prefix for memory operands

--- src/csharp/Intel/Iced/Intel/Assembler/AssemblerMemoryOperandFactory.cs
+++ src/csharp/Intel/Iced/Intel/Assembler/AssemblerMemoryOperandFactory.cs
@@ -2,13 +2,28 @@ namespace Iced.Intel
 {
 	public readonly struct AssemblerMemoryOperandFactory {
 		public AssemblerMemoryOperandFactory(MemoryOperandSize size) {
+			Prefix = Register.None;
 			Size = size;
+		}
+
+		public AssemblerMemoryOperandFactory(MemoryOperandSize size, Register prefix) {
+			Size = size;
+			Prefix = prefix;
 		}
 
 		public readonly MemoryOperandSize Size;
 
-		public AssemblerMemoryOperand this[AssemblerMemoryOperand operand] => new AssemblerMemoryOperand(Size, operand.Prefix, operand.Base, operand.Index, operand.Scale, operand.Displacement);
+		public readonly Register Prefix;
 
-		public AssemblerMemoryOperand this[AssemblerRegister operand] => new AssemblerMemoryOperand(Size, Register.None, operand, Register.None, 0, 0);
+		public AssemblerMemoryOperandFactory es => new AssemblerMemoryOperandFactory(Size, Register.ES); 
+		public AssemblerMemoryOperandFactory cs => new AssemblerMemoryOperandFactory(Size, Register.CS); 
+		public AssemblerMemoryOperandFactory ss => new AssemblerMemoryOperandFactory(Size, Register.SS); 
+		public AssemblerMemoryOperandFactory ds => new AssemblerMemoryOperandFactory(Size, Register.DS); 
+		public AssemblerMemoryOperandFactory fs => new AssemblerMemoryOperandFactory(Size, Register.FS); 
+		public AssemblerMemoryOperandFactory gs => new AssemblerMemoryOperandFactory(Size, Register.GS); 
+		
+		public AssemblerMemoryOperand this[AssemblerMemoryOperand operand] => new AssemblerMemoryOperand(Size, Prefix, operand.Base, operand.Index, operand.Scale, operand.Displacement);
+
+		public AssemblerMemoryOperand this[AssemblerRegister operand] => new AssemblerMemoryOperand(Size, Prefix, operand, Register.None, 0, 0);
 	}
 }
