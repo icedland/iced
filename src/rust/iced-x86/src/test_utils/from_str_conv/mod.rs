21eee4c7be0f7b35f707fd251093f71a46a6ce3e
0xd4d <de4dot@gmail.com>
2020-10-22 18:38:11 +0200

    Remove #[cfg]

--- src/rust/iced-x86/src/test_utils/from_str_conv/mod.rs
+++ src/rust/iced-x86/src/test_utils/from_str_conv/mod.rs
@@ -138,15 +138,6 @@ pub(crate) fn to_u64(value: &str) -> Result<u64, String> {
 	}
 }
 
-#[cfg(any(
-	all(feature = "encoder", feature = "op_code_info"),
-	feature = "instr_info",
-	feature = "gas",
-	feature = "intel",
-	feature = "masm",
-	feature = "nasm",
-	feature = "fast_fmt"
-))]
 pub(crate) fn to_i64(value: &str) -> Result<i64, String> {
 	let mut unsigned_value = value.trim();
 	let mult = if unsigned_value.starts_with('-') {
