020e7941d46219d7c0d1cce7efcad76a3b06643c
0xd4d <de4dot@gmail.com>
2020-12-05 20:37:28 +0100

    Update classifiers (setup.py)

--- src/rust/iced-x86-py/setup.py
+++ src/rust/iced-x86-py/setup.py
@@ -53,6 +53,8 @@ setup(
 		"Programming Language :: Python :: 3.7",
 		"Programming Language :: Python :: 3.8",
 		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy",
 		"Programming Language :: Rust",
 		"Topic :: Software Development :: Disassemblers",
 		"Topic :: Software Development :: Libraries",
