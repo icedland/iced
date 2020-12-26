b17133f4f67c6d9227b779412940f1b65830bf97
0xd4d <de4dot@gmail.com>
2020-12-26 12:36:16 +0100

    Update build file

--- build/ci-py-build-wheels.sh
+++ build/ci-py-build-wheels.sh
@@ -15,16 +15,16 @@ python -m pip install -U setuptools wheel setuptools-rust pytest
 # from earlier builds
 git clean -xdf
 
+cd src/rust/iced-x86-py
+
 # Build the wheel with the minimum supported Python version only
 if python --version 2>&1 | grep "Python 3\.6"; then
 	python setup.py bdist_wheel --py-limited-api=cp36
-
 	mkdir -p /tmp/py-dist
-	cd src/rust/iced-x86-py
 	cp dist/* /tmp/py-dist
 fi
 
 echo "Testing it"
-python -m pip install iced-x86 --no-index -f dist --only-binary :all:
+python -m pip install iced-x86 --no-index -f /tmp/py-dist --only-binary :all:
 python -m pytest --color=yes --code-highlight=yes
 python -m pip uninstall -y iced-x86
