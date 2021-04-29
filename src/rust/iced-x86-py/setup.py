4401c5de27a53848c3fc60892522c28efcd8131e
wtfsck <wtfsck@protonmail.com>
2021-04-29 20:47:02 +0200

    Bump version

--- src/rust/iced-x86-py/setup.py
+++ src/rust/iced-x86-py/setup.py
@@ -24,7 +24,7 @@ py_limited_api = any(arg == "--py-limited-api" or arg.startswith("--py-limited-a
 
 setup(
 	name="iced-x86",
-	version="1.11.1",
+	version="1.11.2",
 	license="MIT",
 	author_email="wtfsck@protonmail.com",
 	author="wtfsck",
