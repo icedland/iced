95fd8ca9168476806af625699b47955525ffcb19
wtfsck <wtfsck@protonmail.com>
2021-07-17 20:20:10 +0200

    Update test

--- src/rust/iced-x86-py/tests/instruction_test.py
+++ src/rust/iced-x86-py/tests/instruction_test.py
@@ -1209,9 +1209,9 @@ def test_fpu_stack_increment_info():
 	assert info.writes_top
 
 def test_pickle():
-	decoder = Decoder(64, b"\xC4\xE3\x49\x48\x10\x41" b"\xC4\xE3\x49\x48\x10\x42", ip=0x1234_5678_9ABC_DEF1)
+	decoder = Decoder(64, b"\xC4\xE3\x49\x48\x10\x41", ip=0x1234_5678_9ABC_DEF1)
 	instr = decoder.decode()
 	dump = pickle.dumps(instr)
 	instr2 = pickle.loads(dump)
 
-	assert instr == instr2
+	assert instr.eq_all_bits(instr2)
