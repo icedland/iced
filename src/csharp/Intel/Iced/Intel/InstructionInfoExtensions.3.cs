69895b1be7404878344e2c06eca2d722bec80921
am0nsec <20334678+am0nsec@users.noreply.github.com>
2021-11-24 23:52:04 +0000

    CSharp fix for #225 and add IsLoop/IsLoopcc members

--- src/csharp/Intel/Iced/Intel/InstructionInfoExtensions.3.cs
+++ src/csharp/Intel/Iced/Intel/InstructionInfoExtensions.3.cs
@@ -18,6 +18,33 @@ namespace Iced.Intel {
 		public static bool IsJccShortOrNear(this Code code) =>
 			(uint)(code - Code.Jo_rel8_16) <= (uint)(Code.Jg_rel8_64 - Code.Jo_rel8_16) ||
 			(uint)(code - Code.Jo_rel16) <= (uint)(Code.Jg_rel32_64 - Code.Jo_rel16);
+
+		/// <summary>
+		/// Checks if it's a <c>Jcxz SHORT</c>, <c>Jecxz SHORT</c> or <c>Jrcxz SHORT</c> instruction
+		/// </summary>
+		/// <param name="code">Code value</param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsJcxShort(this Code code) =>
+			(uint)(code - Code.Jcxz_rel8_16) <= (uint)(Code.Jrcxz_rel8_64 - Code.Jcxz_rel8_16);
+
+		/// <summary>
+		/// Checks if it's a <c>LOOPcc SHORT</c> instruction
+		/// </summary>
+		/// <param name="code">Code value</param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsLoopcc(this Code code) =>
+			(uint)(code - Code.Loopne_rel8_16_CX) <= (uint)(Code.Loope_rel8_64_RCX - Code.Loopne_rel8_16_CX);
+
+		/// <summary>
+		/// Checks if it's a <c>LOOP SHORT</c> instruction
+		/// </summary>
+		/// <param name="code">Code value</param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsLoop(this Code code) => 
+			(uint)(code - Code.Loop_rel8_16_CX) <= (uint)(Code.Loop_rel8_64_RCX - Code.Loop_rel8_16_CX);
 	}
 }
 #endif
