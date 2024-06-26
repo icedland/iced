a9278ea02330f2a94ee304e47c0d19520bb1c967
TetzkatLipHoka <TLH@freenet.de>
2024-06-26 04:01:56 +0200

    Initial

---
+++ src/Delphi/Samples/CreateAndEncode/CreateAndEncode.dpr
@@ -0,0 +1,173 @@
+program DisassembleGlobal;
+
+{$APPTYPE CONSOLE}
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  SysUtils,
+
+  uIced.Types in '..\..\uIced.Types.pas',
+  uIced.Imports in '..\..\uIced.Imports.pas',
+  uIced in '..\..\uIced.pas';
+
+{
+This example produces the following output:
+00001248FC840000 55                push    %rbp
+00001248FC840001 57                push    %rdi
+00001248FC840002 56                push    %rsi
+00001248FC840003 4881EC50000000    sub     $0x50,%rsp
+00001248FC84000A C5F877            vzeroupper
+00001248FC84000D 488D6C2460        lea     0x60(%rsp),%rbp
+00001248FC840012 488BF1            mov     %rcx,%rsi
+00001248FC840015 488D7DC8          lea     -0x38(%rbp),%rdi
+00001248FC840019 B90A000000        mov     $0xA,%ecx
+00001248FC84001E 33C0              xor     %eax,%eax
+00001248FC840020 F3AB              rep stos %eax,(%rdi)
+00001248FC840022 4881FE78563412    cmp     $0x12345678,%rsi
+00001248FC840029 7501              jne     0x00001248FC84002C
+00001248FC84002B 90                nop
+00001248FC84002C 4533FF            xor     %r15d,%r15d
+00001248FC84002F 4C8D3501000000    lea     0x1248FC840037,%r14
+00001248FC840036 90                nop
+00001248FC840037 12345678          .byte   0x12,0x34,0x56,0x78
+}
+const
+  HEXBYTES_COLUMN_BYTE_LENGTH       = 16;
+  EXAMPLE_CODE_BITNESS              = bt64;
+  EXAMPLE_CODE_RIP                  = $00001248FC840000;
+  raw_data : Array [ 0..3 ] of Byte = ( $12, $34, $56, $78 );
+var
+  Instruction   : TInstruction;
+  MemoryOperand : TMemoryOperand;
+  Instructions  : TInstructionList;
+  Results       : TBlockEncoderResult;
+  Data          : PByte;
+  S             : String;
+  i             : Integer;
+  label1        : UInt64;
+  data1         : UInt64;
+begin
+  if NOT IsInitDLL then
+    begin
+    WriteLn( 'Library not loaded.' );
+    WriteLn( 'Press enter to exit.' );
+    ReadLn;
+    Exit;
+    end;
+
+  Instructions := TInstructionList.Create;
+  // All created instructions get an IP of 0. The label id is just an IP.
+  // The branch instruction's *target* IP should be equal to the IP of the
+  // target instruction.
+  {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  label1 := Instructions.CreateLabel;
+  {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+
+  Instructions.Add( Instruction.with1( Push_r64, RBP ) );
+  Instructions.Add( Instruction.with1( Push_r64, RDI ) );
+  Instructions.Add( Instruction.with1( Push_r64, RSI ) );
+  Instructions.Add( Instruction.with2( Sub_rm64_imm32, RSP, Cardinal( $50 ) ) );
+  Instructions.Add( Instruction.with_( VEX_Vzeroupper ) );
+  Instructions.Add( Instruction.with2(
+      Lea_r64_m,
+      RBP,
+      MemoryOperand.with_base_displ( RSP, $60 )
+   ) );
+  Instructions.Add( Instruction.with2( Mov_r64_rm64, RSI, RCX ) );
+  Instructions.Add( Instruction.with2(
+      Lea_r64_m,
+      RDI,
+      MemoryOperand.with_base_displ( RBP, -$38 )
+   ) );
+  Instructions.Add( Instruction.with2( Mov_r32_imm32, ECX, Cardinal( $0A ) ) );
+  Instructions.Add( Instruction.with2( Xor_r32_rm32, EAX, EAX ) );
+  Instructions.Add( Instruction.with_rep_stosd( Cardinal( EXAMPLE_CODE_BITNESS ) ) );
+  Instructions.Add( Instruction.with2( Cmp_rm64_imm32, RSI, Cardinal( $12345678 ) ) );
+  // Create a branch instruction that references label1
+  Instructions.Add( Instruction.with_branch( Jne_rel32_64, label1 ) );
+  Instructions.Add( Instruction.with_( Nopd ) );
+  // Add the instruction that is the target of the branch
+  Instructions.Add( Instruction.with2( Xor_r32_rm32, R15D, R15D ), label1 );
+
+  // Create an instruction that accesses some data using an RIP relative memory operand
+  {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  data1 := Instructions.CreateLabel;
+  {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  Instructions.Add( Instruction.with2(
+        Lea_r64_m,
+        R14,
+        MemoryOperand.with_base_displ( RIP, Int64( data1 ) )
+   ) );
+  Instructions.Add( Instruction.with_( Nopd ) );
+  Instructions.Add( Instruction.with_declare_byte( @raw_data[ 0 ], Length( raw_data ) ), data1 );
+
+  // Use BlockEncoder to encode a block of instructions. This block can contain any
+  // number of branches and any number of instructions. It does support encoding more
+  // than one block but it's rarely needed.
+  // It uses Encoder to encode all instructions.
+  // If the target of a branch is too far away, it can fix it to use a longer branch.
+  // This can be disabled by enabling some BlockEncoderOptions flags.
+
+  Iced.BlockEncoder.Bitness := EXAMPLE_CODE_BITNESS;
+  {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  Iced.BlockEncoder.Encode( EXAMPLE_CODE_RIP, Instructions, Results );
+  {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  Instructions.Free;
+
+  // Now disassemble the encoded instructions. Note that the 'jmp near'
+  // instruction was turned into a 'jmp short' instruction because we
+  // didn't disable branch optimizations.
+  Iced.Decoder.Bitness := EXAMPLE_CODE_BITNESS;
+
+  Data := PByte( Results.code_buffer );
+  {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  Iced.Decoder.SetData( PByte( Results.code_buffer ), Results.code_buffer_len-Length( raw_data ), EXAMPLE_CODE_RIP{Results.RIP}, doNONE );
+  {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+
+  Iced.Formatter.FormatterType         := ftGas;
+  Iced.Formatter.FirstOperandCharIndex := 8;
+
+  while Iced.Decoder.CanDecode do
+    begin
+    Iced.Decoder.Decode( Instruction );
+
+    // Assembly
+    S := Format( '%.16x ', [ Instruction.RIP ] );
+
+    for i := 0 to Instruction.len-1 do
+      begin
+      S := S + Format( '%.2x', [ Data^ ] );
+      Inc( Data );
+      end;
+
+    for i := 0 to HEXBYTES_COLUMN_BYTE_LENGTH-Instruction.len*2+1 do
+      S := S + ' ';
+
+    WriteLn( S + Iced.Formatter.FormatToString( Instruction ) );
+    end;
+
+//  Data := PByte( Results.code_buffer ); // @raw_data[ 0 ];
+//  Inc( Data, Results.code_buffer_len-Length( raw_data ) );
+  Instruction.from_declare_byte( Data, Length( raw_data ) );
+  Instruction.len := Length( raw_data );
+  S := Format( '%.16x ', [ Iced.Decoder.IP ] );
+  for i := 0 to Instruction.len-1 do
+    begin
+    S := S + Format( '%.2x', [ Data^ ] );
+    Inc( Data );
+    end;
+  for i := 0 to HEXBYTES_COLUMN_BYTE_LENGTH-Instruction.len*2+1 do
+    S := S + ' ';
+  WriteLn( S + Iced.Formatter.FormatToString( Instruction ) );
+
+//  Iced.Decoder.SetIP( Decoder, EXAMPLE_RIP );
+//  Iced.Decoder.SetPosition( Decoder, 0 );
+
+  WriteLn( 'Press enter to exit.' );
+  ReadLn;
+
+end.
---
+++ src/Delphi/Samples/DisassembleLocal/DisassembleLocal.dpr
@@ -0,0 +1,99 @@
+program DisassembleLocal;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+
+  uIced.Types in '..\..\uIced.Types.pas',  
+  uIced.Imports in '..\..\uIced.Imports.pas',
+  uIced in '..\..\uIced.pas';
+
+{
+This example produces the following output:
+00007FFAC46ACDA4 48895C2410           mov       [rsp+10h],rbx
+00007FFAC46ACDA9 4889742418           mov       [rsp+18h],rsi
+00007FFAC46ACDAE 55                   push      rbp
+00007FFAC46ACDAF 57                   push      rdi
+00007FFAC46ACDB0 4156                 push      r14
+00007FFAC46ACDB2 488DAC2400FFFFFF     lea       rbp,[rsp-100h]
+00007FFAC46ACDBA 4881EC00020000       sub       rsp,200h
+00007FFAC46ACDC1 488B0518570A00       mov       rax,[rel 7FFA`C475`24E0h]
+00007FFAC46ACDC8 4833C4               xor       rax,rsp
+00007FFAC46ACDCB 488985F0000000       mov       [rbp+0F0h],rax
+00007FFAC46ACDD2 4C8B052F240A00       mov       r8,[rel 7FFA`C474`F208h]
+00007FFAC46ACDD9 488D05787C0400       lea       rax,[rel 7FFA`C46F`4A58h]
+00007FFAC46ACDE0 33FF                 xor       edi,edi
+}
+
+const
+  HEXBYTES_COLUMN_BYTE_LENGTH            = 10;
+  EXAMPLE_CODE_BITNESS                   = bt64;
+  EXAMPLE_CODE_RIP                       = $00007FFAC46ACDA4;
+  EXAMPLE_CODE : Array [ 0..61 ] of Byte = ( $48, $89, $5C, $24, $10, $48, $89, $74, $24, $18, $55, $57, $41, $56, $48, $8D,
+                                             $AC, $24, $00, $FF, $FF, $FF, $48, $81, $EC, $00, $02, $00, $00, $48, $8B, $05,
+                                             $18, $57, $0A, $00, $48, $33, $C4, $48, $89, $85, $F0, $00, $00, $00, $4C, $8B,
+                                             $05, $2F, $24, $0A, $00, $48, $8D, $05, $78, $7C, $04, $00, $33, $FF
+                                           );
+var
+  Decoder     : TIcedDecoder;
+  Formatter   : TIcedFormatter;
+  Instruction : TInstruction;
+  sOutput     : String;
+  start_index : Integer;
+  i           : Integer;
+begin
+  if NOT IsInitDLL then
+    begin
+    WriteLn( 'Library not loaded.' );
+    WriteLn( 'Press enter to exit.' );
+    ReadLn;
+    Exit;
+    end;
+
+  Decoder := TIcedDecoder.Create( EXAMPLE_CODE_BITNESS );
+
+  {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+  Decoder.SetData( @EXAMPLE_CODE[ 0 ], Length( EXAMPLE_CODE ), EXAMPLE_CODE_RIP, doNONE );
+  {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+
+  // Formatters: Masm*, Nasm*, Gas* (AT&T) and Intel* (XED).
+  // For fastest code, see `SpecializedFormatter` which is ~3.3x faster. Use it if formatting
+  // speed is more important than being able to re-assemble formatted instructions.
+  Formatter := TIcedFormatter.Create( ftNasm );
+
+  // Change some options, there are many more
+  Formatter.DigitSeparator        := '`';
+  Formatter.FirstOperandCharIndex := 10;
+
+  while Decoder.CanDecode do
+    begin
+    Decoder.Decode( Instruction );
+
+    // Eg. "00007FFAC46ACDB2 488DAC2400FFFFFF     lea       rbp,[rsp-100h]"
+
+    // Hex
+    {$IF CompilerVersion < 23}{$RANGECHECKS OFF}{$IFEND} // RangeCheck might cause Internal-Error C1118
+    start_index := instruction.RIP-EXAMPLE_CODE_RIP;
+    {$IF CompilerVersion < 23}{$RANGECHECKS ON}{$IFEND} // RangeCheck might cause Internal-Error C1118
+    sOutput := '';
+    for i := start_index to start_index+Instruction.len-1 do
+      sOutput := sOutput + IntToHex( EXAMPLE_CODE[ i ], 2 );
+
+    if ( Instruction.len < HEXBYTES_COLUMN_BYTE_LENGTH ) then
+      begin
+      for i := 0 to HEXBYTES_COLUMN_BYTE_LENGTH-Instruction.len-1 do
+        sOutput := sOutput + '  '
+      end;
+
+    sOutput := Format( '%.16x ', [ instruction.RIP ] ) + sOutput + Formatter.FormatToString( Instruction );
+
+    WriteLn( sOutput );
+    end;
+  Decoder.free;
+  Formatter.free;
+
+  WriteLn( 'Press enter to exit.' );
+  ReadLn;
+
+end.
